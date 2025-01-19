729cc1d2b944063b1b4b40beb14954c91312f32a
adhikaripraveen <adhikariP861@gmail.com>
2025-01-19 16:47:03 +0530

    changes

--- Movix-Backend/app.js
+++ Movix-Backend/app.js
@@ -18,10 +18,11 @@ const corsOptions = {
 
 const app = express();
 app.use(express.static(path.join(__dirname,'../movix-frontend', 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../movix-frontend/build', 'index.html'));
-});
-console.log(path.join(__dirname,'../movix-frontend', 'build'))
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../movix-frontend/build', 'index.html'));
+// });
+console.log('Resolved build path:', path.join(__dirname, '../movix-frontend/build'));
+
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
